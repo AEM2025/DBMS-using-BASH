5e4470969af75df2635f96426be7e0fcca9d9fda
Ahmed Emad <emada0471@gmail.com>
2023-03-26 00:32:52 +0200

    Add mainmenu

---
+++ Mainmenu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SP3="Please enter your choice"
+
+echo "Enter choices from 1 to 5"
+options=("Create Database" "List Databases" "Connect To Databases" "Drop Database" "exit")
+
+while [[ "$REPLY" != "exit" ]]
+do
+	select answer in "${options[@]}"
+	do
+		case $REPLY in
+			1) echo "Create DB" ;;
+			2) echo "List DBs" ;;
+			3) echo "Connect to DBs"
+			       . ./Connect_DB.sh	;;
+			4) echo "Drop DBs" ;;
+			5) exit;;
+			*) echo "Invalid input please try again later" ;;
+		esac
+	done
+done
