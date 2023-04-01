67453c2e30c6320297ae00f8596cab7dc116441d
Ahmed Emad <emada0471@gmail.com>
2023-04-01 20:53:10 +0200

    Add select_row function

--- Select_Table.sh
+++ Select_Table.sh
@@ -1,41 +1,46 @@
 #!/bin/bash
 
-DB_Name="Company"
-
+DB_Name=$1
 error=""
+DIR="./DataBases/$DB_Name"
 
-#read -rp "Please enter table name:  " Table_Name
-Table_Name="Phones"
+# Enter table name
+read -rp "Please enter table name:  " Table_Name
 
+# check if table exist or not first
 function Table_exist {
-	if [ -f ./DataBases/$DB_Name/$Table_Name.DATA -a -f ./DataBases/$DB_Name/$Table_Name.metadata ]
+	if [ -f $DIR/$Table_Name.DATA -a -f $DIR/$Table_Name.metadata ]
 	then
-		echo "Table exist and everything good"
 		error="0"
 	else
 		echo "Table not exist !!!"
 		error="1"
 	fi
 }
+
+# Select all
 function Select_All {
-	echo "-------------------$DB_Name"
-	echo "-------------------$Table_Name"
-	cat ./DataBases/$DB_Name/$Table_Name.DATA
+	cat $DIR/$Table_Name.DATA
 }
 
-#function Select_Row {}
-#function Select_Column {}
+# Select specific row
+function Select_Row {
 
+Data_Num=$(cat $DIR/$Table_Name.DATA | wc -l)
+echo "We have $Data_Num in this table"
+echo ""
+read -rp "Enter number of row that you want: " row
+if [ $row -lt $Data_Num ]
+then
+	data=$(cat $DIR/$Table_Name.DATA | sed -n "$row p" )
+	echo "$data"
+else
+	echo "Enter numbers between 1 and $Data_Num"
+fi
+}
 
-
-
-
-
-
-
-
-
-
+# Select specific column
+# function Select_Column {}
 
 options=("Select all" "Select row" "Select Column" "Exit")
 echo "Enter choice from 1 to 4"
@@ -44,20 +49,24 @@ while [[ $REPLY != "Exit" ]]
 do
 	select choice in "${options[@]}"
 	do
-
 		case $REPLY in
 	
 			1) echo "select everything"
 				Table_exist
-
 				if [[ $error = 0 ]]
 				then
 					Select_All
 				fi
 				;;
-			2) echo "select specific row" ;;
+			2) echo "select specific row"
+			       Table_exist
+			       if [[ $error = 0 ]]
+			       then
+				       Select_Row
+			       fi
+				;;
 			3) echo "select specific column" ;;
-			4) exit;;
+			4) exit ;;
 			*) echo "please enter valid option";;
 		esac
 	done
