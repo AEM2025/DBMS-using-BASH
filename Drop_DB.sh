2ed265634b9c333285832c03e2b2de09d774a35a
Ahmed Emad <emada0471@gmail.com>
2023-04-04 00:29:18 +0200

    Edit Drop_DB file and add validation function

---
+++ Drop_DB.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+
+# Test Cases:
+# 1 - [Done...] Check if Databases file exist or not.
+# 2 - [Done...] Check if DB is already exist before removing it.
+
+read -rp "Enter the name of db you want to remove: " db_name;
+
+# we use this variable as flag to check everything is ok.
+error=""
+
+# function for validations
+function Validation {
+	if [ -d "./DataBases" ]
+	then
+		error="0"
+	else
+		error="1"
+		echo "Error! DataBases file not exist ❌"
+	fi
+
+	if [ -d "./DataBases/$db_name" ]
+	then
+		error="0"
+	else
+		error="1"
+	       	echo "Error! No Database with this name: $db_name ❌"
+
+	fi
+}
+
+
+# Call function validation first 
+Validation
+
+if [ $error = 0 ]
+then
+       	rm -r ./DataBases/$db_name 
+	echo "Database: $db_name deleted successfully ✓ ✓"
+else
+
+	echo "Can't complete this action ❌❌"
+fi
