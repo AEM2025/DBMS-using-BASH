90e922921bf6c75cfaeef0152dd0adfdc2f8ef3a
Ahmed Emad <emada0471@gmail.com>
2023-04-04 22:04:23 +0200

    Add validation function

--- Connect_DB.sh
+++ Connect_DB.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 
-#SP3="Please enter your choice"
-
 # Test Cases:
-# [Done] 1 - If DB not exist
-# 2 - Change select menu to be more readable
+# [Done..] 1 - Check if DataBases folder not exist.
+# [Done..] 2 - If DB not exist
 
 read -rp "Please enter database that you want to connect: " DB_Name
 
-Check=$(find ./DataBases/$DB_Name -type d)
+error=""
+function validation {
+	
+	# 1 - Check if DataBases folder not exist.
+	if [ -d ./DataBases ]
+	then
+		error="0"
+	else
+		error="1"
+		echo "DataBases file not exist"
+	fi
+
+	# 2 - This Condition to test if DB exist or not
+	Check=`find ./DataBases/$DB_Name -type d`
+	if [[ $Check = "" ]]
+	then
+		error="1"
+		echo "There's no DB with this name: $DB_Name"
+	else
+		error="0"
+	fi
+}
+
+
 
 options=("Create Table" "List Tables" "Drop Table" "Insert into Table" "Select From Table" "Delete From Table" "Update Table" "Back" "Exit")
 
+# Call validation function
+validation
 
-# This Condition to test if DB exist or not
-
-if [[ $Check != "" ]]
+if [ $error -eq 0 ]
 then
 	echo "Enter choices from 1 to 9"
 	while [[ "$REPLY" != "Exit" ]]
@@ -47,8 +68,7 @@ then
 			esac
 		done
 	done
-
 else
-	echo "This DB not exist!!!!!!!!!!!!!!!!"
+	echo "Can't complete this action ❌❌"
 fi
 
