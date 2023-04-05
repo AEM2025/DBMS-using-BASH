7dd42d9663e11497e25253618eeaff3855e01829
Ahmed Emad <emada0471@gmail.com>
2023-04-05 02:57:51 +0200

    Fix bug and edit some conditions

--- Drop_Table.sh
+++ Drop_Table.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Test Cases:
-# 1 - Check if DataBases file exist.
-# 2 - Check if DB file is exist.
-# 3 - Check if table exist.
+# [Done..] 1 - Check if DataBases file exist.
+# [Done..] 2 - Check if DB file is exist.
+# [Done..] 3 - Check if table exist.
 
-$DB_Name=$1
+DB_Name=$1
 read -rp "Enter table that you want to remove: " tableName
 
 # We use this variables as flag. 
@@ -18,22 +18,25 @@ function validation {
         then
 		# if this folder exist, go to it. pwd DataBases/
 		error="0"
-		cd ./DataBases
+		#echo " 1 ------------------ $error"
         else
                 error="1"
+		#echo "1------------------- $error "
                 echo "Error! DataBases file not exist ❌"
         fi
 
 	# 2 - Check if DB file is exist.
-	# Note: we can use $DB_Name instead of ./DataBases/$DB_Name but keep it for safe.
-
+	# Note: - We can use $DB_Name instead of ./DataBases/$DB_Name but keep it for safe.
+	# 	- We must check if the error variable is still 0. 
+	# 	- Otherwise, we will override the value of it and may cause an errors.
+	
 	if [ -d "./DataBases/$DB_Name"  -a $error = 0 ]
         then
-		# if this folder exist, go to it. pwd DataBases/Db_Name
                 error="0"
-		cd $DB_Name
+		#echo "2--------------------- $error"
         else
                 error="1"
+		#echo "2 -------------------------$error"
                 echo "Error! No DB with this name: $DB_Name ❌"
         fi
 
@@ -42,17 +45,18 @@ function validation {
 	if [ -f ./DataBases/$DB_Name/$tableName.DATA -a -f ./DataBases/$DB_Name/$tableName.metadata -a $error = 0 ]
 	then
 		error="0"
+		#echo "3---------------------- $error" 
 	else
 		error="1"
-	       	echo "Error! This table already exist ❌"
+		#echo "3------------------------ $error"
+	       	echo "Error! This table not exist ❌"
 	fi
 }
 
 # call function here.
 validation
 
-# Check if all variables are 0
-
+# Check if error is 0
 if [ $error = 0 ]
 then
 	rm ./DataBases/$DB_Name/$tableName.DATA
