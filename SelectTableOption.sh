9a604699210ae5f6c00c4d5b789d672052b172ae
abdelrahman metwally <93939024+abdelrahman5536025@users.noreply.github.com>
2023-04-06 03:40:19 +0200

    update on select from table

--- SelectTableOption.sh
+++ SelectTableOption.sh
@@ -1,34 +1,71 @@
-#!/bin/bash
+ #!/usr/bin/bash
+export LC_COLLATE=C
+shopt -s extglob 
 
-SP3="Please enter your choice"
-
-echo "Enter the modification you want to do on the table"
-options=("drop table" "insert into table" "select form table" "delete form table" "update table" "exit ")
-
-
-while [[ "$REPLY" != "exit" ]]
+read -p "Enter the db name:" DB
+read -p "Enter table name:" tableName
+while true
 do
-	select answer in "${options[@]}"
-	do
-		case $REPLY in
-			1) echo "drop table"
-			       . ./Drop_Table.sh      
-				 ;;
-			2) echo "insert into table"
-			      . ./ 	
-					;;
-			3) echo "select form table"
-			       . ./
-			       ;;
-			4) echo "delete form table" 
-				. ./
-					 ;;
-			5) echo "update table"
-				. ./  ;;
-			6) exit
-				;;
-			*) echo "Invalid input please try again later"
-			       	;;
-		esac
-	done
+if [[ -f "DataBases/$DB/$tableName.DATA" ]];then
+     
+    
+    select choice  in "Select All" "Select Column" "Select by column"
+    do
+        case $choice in
+        "Select All" )
+            cat "DataBases/$DB/$tableName.DATA"
+            echo -n ""
+        ;;
+        "Select Column" )
+            nf=`awk -F : '{if(NR==1){print  NF'}} "DataBases/$DB/$tableName.DATA"`
+            for i in $(seq 1 $nf)
+            do
+                
+                allcolumn=`awk -F : '{if(NR==1){print $i}}' "DataBases/$DB/$tableName.DATA"`
+                singlecolumn=`awk -F : -v x=$i '{if(NR==1){print $i}}' "DataBases/$DB/$tableName.DATA"`
+                echo "the table columns are : "$allcolumn
+                read -p "Enter column name:" column
+                colname=`sed -n '1p' "DataBases/$DB/$tableName.DATA" | grep -o $column `
+                colnum=`cat "DataBases/$DB/$tableName.DATA" | awk -F : -v b="$column" '{for (i=1;i<=NF;i++) {if($i == b){print i} }}'`
+                if [[ $column  ==  $colname ]]; then
+                    cut -d ":" -f $colnum "DataBases/$DB/$tableName.DATA"
+                    break
+                    
+                else
+                    echo "this column doesn't exist"
+                    break
+                fi
+            done
+            
+        ;;
+        "Select by column" )
+             nf=`awk -F : '{if(NR==1){print  NF'}} "DataBases/$DB/$tableName.DATA"`
+            for i in $(seq 1 $nf)
+            do
+                
+                allcolumn=`awk -F : '{if(NR==1){print $i}}' "DataBases/$DB/$tableName.DATA"`
+                singlecolumn=`awk -F : -v x=$i '{if(NR==1){print $i}}' "DataBases/$DB/$tableName.DATA"`
+                echo "the table columns are : "$allcolumn
+                read -p "Enter column name:" column
+                
+                colname=`sed -n '1p' "DataBases/$DB/$tableName.DATA" | grep -o $column `
+                colnum=`cat "DataBases/$DB/$tableName.DATA" | awk -F : -v b="$column" '{for (i=1;i<=NF;i++) {if($i == b){print i} }}'`
+                if [[ $column  ==  $colname ]]; then
+                    read -p "Enter the value you want: " word
+                    sed -n "/$word/p" "DataBases/$DB/$tableName.DATA" 
+                    break
+                    
+                else
+                    echo "this column doesn't exist"
+                    break
+                fi
+            done
+        ;;
+        esac
+    done
+else
+    echo "this table doen't exist "
+    read -p "Enter table name:" tablename
+
+fi
 done
