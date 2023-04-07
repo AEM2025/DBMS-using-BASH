10a396326f8c8b6e08faee819e399feaee84d1a8
abdelrahman metwally <93939024+abdelrahman5536025@users.noreply.github.com>
2023-04-07 07:35:04 +0200

    some modifications to the code

--- Update_Table.sh
+++ Update_Table.sh
@@ -21,24 +21,24 @@ else
     updateTable
 
     else
-      echo -e "Enter FIELD name to set: \c"
-      read setField
-      setFid=$(awk 'BEGIN{FS=":"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$setField'") print i}}}' "DataBases/$DB/$tableName.DATA")
-      if [[ $setFid == "" ]]
-      then
-        echo " Not Found, try again"
-       updateTable
+    #  echo -e "Enter FIELD name to set: \c"
+     # read setField
+      #setFid=$(awk 'BEGIN{FS=":"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$setField'") print i}}}' "DataBases/$DB/$tableName.DATA")
+      #if [[ $setFid == "" ]]
+      #then
+       # echo " Not Found, try again"
+       #updateTable
 
-      else
+      #else
         echo -e "Enter new Value to set: \c"
         read newValue
         NR=$(awk 'BEGIN{FS=":"}{if ($'$fid' == "'$val'") print NR}' "DataBases/$DB/$tableName.DATA" 2>>./.error.log)
-        oldValue=$(awk 'BEGIN{FS=":"}{if(NR=='$NR'){for(i=1;i<=NF;i++){if(i=='$setFid') print $i}}}' "DataBases/$DB/$tableName.DATA" 2>>./.error.log)
+        oldValue=$(awk 'BEGIN{FS=":"}{if(NR=='$NR'){for(i=1;i<=NF;i++){if(i=='$fid') print $i}}}' "DataBases/$DB/$tableName.DATA" 2>>./.error.log)
         echo $oldValue
         sed -i ''$NR's/'$oldValue'/'$newValue'/g' "DataBases/$DB/$tableName.DATA" 2>>./.error.log
         echo "Row Updated Successfully"
         updateTable
-      fi
+      #fi
     fi
   fi
 }
@@ -46,5 +46,5 @@ else
 
 #cut -d: -f1 "DataBases/$DB/$tableName.metadata" | xargs |sed -e 's/ /:/g'>"DataBases/$DB/$tableName.DATA"
 
-echo "hello "
+#echo "hello "
 updateTable
