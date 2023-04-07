2f5e1fb83f61e688794b5a61989f5ea525637c28
Abdelrahman Metwally <abdelrahmanmetwally23@gmail.com>
2023-04-07 19:48:41 +0200

    Add validation function & check pk unique or not

--- Insert_Into_Table.sh
+++ Insert_Into_Table.sh
@@ -20,6 +20,9 @@ Test Cases:
 read -rp "Enter table name " tableName
 DB_Name=$1
 error=""
+dataTypeError="0"
+unique="0"
+row=""
 
 # function for validations
 function Validation {
@@ -43,73 +46,115 @@ function Validation {
 	fi
 }
 
+function DataTypeCheck {
+	
+	Col_dt=$1
+	Col_value=$2
 
-# new string to save data to it
-row=""
+	# Must strat ^ and must end $ one or more +
+	# REGEX to test numbers and strings
+	intRegex="^[0-9]+$"
+	strRegex="^[a-zA-Z]+$"
 
-# This flag will be 1 if there's any error in values
-dataTypeError="0"
+	if [ -z "$Col_value" -o "$Col_value" = " " ]
+	then
+		dataTypeError="1"
+		echo "Error! value can't be empty ❌"
+	fi
+
+
+	if [[ $Col_dt = "n" || $Col_dt = "N" ]]
+	then
+		if ! [[ $Col_value =~ $intRegex ]]
+		then
+			dataTypeError="1"
+			echo "Error! value must be number ❌"
+		fi
+	fi
+
+	# if column is string, check regex and If there's any error set flag 1
+	if [[ $Col_dt = "s" || $Col_dt = "S" ]]
+	then
+		if ! [[ $Col_value =~ $strRegex ]]
+		then
+			dataTypeError="1"
+			echo "Error! value must be characters from A-Z only ❌"
+		fi
+	fi
+}
+
+function PK {
+
+	#echo " pk function started"
+	Col_value=$1
+	Col_pk=$2
+	# Check unique for primary key.
+	#echo "it's primary key"
+
+	arr=( $(cat "./DataBases/$DB_Name/$tableName.DATA" | cut -d ':' -f '1') )
+	for i in "${!arr[@]}"
+	do     
+		if [ $Col_value = "${arr[$i]}" ]
+		then 
+			unique="1"
+			echo "Error!! primary key must be unique ❌"
+		fi      
+		#echo "line number ----------------- ${arr[$i]}"
+	done
+}
+
 
 Validation
 
 # ${!lines[@]} ----> to read index of this item
+# Important Note: if we use i in for loop again, it will start from the previous value at the function PK
 
 if [ $error = 0 ]
 then
 	IFS=$'\n' read -d '' -r -a lines < "./DataBases/$DB_Name/$tableName.metadata"
 
-	for i in "${!lines[@]}"
+	for l in "${!lines[@]}"
 	do
 		# Split line based on : and save each column in a variable 
-		IFS=':' read -r -a column <<< "${lines[i]}"
+		IFS=':' read -r -a column <<< "${lines[l]}"
 		Col_Name=${column[0]}
 		Col_dt=${column[1]}
 		Col_pk=${column[2]}
 
 		read -rp "Enter value for column $Col_Name " Col_value
 	
-		# Must strat ^ and must end $ one or more +
-		# REGEX to test numbers and strings
-
-		intRegex="^[0-9]+$"
-		strRegex="^[a-zA-Z]+$"
-
-		# if it's number, check regex and display an error message and set flag 1
-
-		if [[ $Col_dt = "n" || $Col_dt = "N" ]]
+		if [[ $Col_pk = "Yes" ]]
 		then
-			if ! [[ $Col_value =~ $intRegex ]]
-			then
-				dataTypeError="1"
-				echo "Error: value must be number ❌"
-			fi
-		fi
-	
-		# if column is string, check regex and If there's any error set flag 1
-
-		if [[ $Col_dt = "s" || $Col_dt = "S" ]]
-		then
-			if ! [[ $Col_value =~ $strRegex ]]
-			then
-				dataTypeError="1"
-				echo "Error: value contain special character ❌"
-			fi
+			PK $Col_value $Col_pk
 		fi
 
-		if [[ $dataTypeError = 0 ]]
+		DataTypeCheck $Col_dt $Col_value
+
+		#echo " ----------------- $dataTypeError"
+		#echo " ------------------$unique"
+		#echo " -----------------$l"
+		
+		if [[ $dataTypeError = 0 &&  $unique = 0 ]]
 		then
-			if [ $i -eq 0 ]
+
+			if [ $l -eq 0 ]
 			then
 				row=$Col_value
+				#echo " ------------------------- $row"
 			else
-                                row="$row:$Col_value"
+				#echo " else -------------------- $row"
+	    			row="$row:$Col_value"
 			fi
 		fi
 	done
 
-	if [[ $dataTypeError = 0 ]]
+	#echo "-----------------$row"
+	#echo "--------------------$dataTypeError"
+	#echo "-------------------------$unique"
+
+	if [[ $dataTypeError = 0  && $unique = 0 ]]
 	then
-		echo $row >> ./DataBases/$DB/$tableName.DATA
+		echo $row >> "./DataBases/$DB_Name/$tableName.DATA"
 	else
 		echo "Can't complete this action ❌"
 	fi
